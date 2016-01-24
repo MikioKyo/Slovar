7466764608ab201f37e6c351d37c384280cdce29
Mikio <mih.chumak@yandex.ru>
2016-01-24 21:43:15 +0800

    Сделал список, кнопку выхода

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QStringList>
+#include <QString>
+#include <QDir>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
@@ -7,5 +13,11 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
+    QStringList nameFilter("*.txt");
+    QDir directory("C:/Users/Mikio/Desktop/slovar/slbase");
+    QStringList txtFilesAndDirectories = directory.entryList(nameFilter);
+    foreach (QString str, txtFilesAndDirectories) {
+        // cout << str.toLocal8Bit().constData() << endl;
+    }
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QString>
+#include <QDir>
+#include <iostream>
+#include <cstdlib>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QStringList nameFilter("*.txt");
+    QDir directory("C:/Users/Mikio/Desktop/slovar/slbase");
+    QStringList txtFilesAndDirectories = directory.entryList(nameFilter);
+    foreach (QString str, txtFilesAndDirectories) {
+        //cout << str.toLocal8Bit().constData() << endl;
+        ui->listWidget->addItem(str);
+    }
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_exitButton_clicked()
+{
+    exit(0);
 }
