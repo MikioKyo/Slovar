a5dd5f783829c2c12de63d170a4b527ba1c944e5
Mikio <mih.chumak@yandex.ru>
2016-03-27 15:49:47 +0900

    Переключение строки посредством нажатия кнопки

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -67,7 +67,7 @@ void MainWindow::on_listWidget_itemClicked(QListWidgetItem *item)
         int qrand();
         bool buttonOn;
         buttonOn = false;
-        QObject::connect(&answerButton, SIGNAL(clicked()), &buttonOn, SLOT());
+        QObject::connect(&ui->answerButton, SIGNAL(clicked()), &buttonOn, SLOT());
         QCoreApplication::processEvents();
 
 }
