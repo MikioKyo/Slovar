74cd8f1db2190bb8d8d9a7a07464760a521a70f0
MikioKyo <mih.chumak@yandex.ru>
2017-11-03 22:37:37 +0800

    Сделана проверка и шаффл ответов

--- main.py
+++ main.py
@@ -1,67 +1,85 @@
-# import the library
+﻿# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
 from appJar import gui
 from os import listdir
- 
+import random
+
 # grid tip
 # "row=1 column=2 colspan=1 rowspan=2", 1, 2, 1, 2)
 # handle button events
 lineNumber = 1
+rightAnswer = ""
 rightAnswerCount = 0
 wrongAnswerCount = 0
 name = ""
 # otv = []
-exerciseHeader = ""
 exerciseLines = []
+
+
 def selectExercise(button):
     global name
     global otv
-    global exerciseHeader
     global exerciseLines
+    global rightAnswer
+
+    exerciseHeader = ""
     name = app.getListItems("list")[0]
-    app.setLabel("header", name[:-4])
-    # otv=open(name, 'r', encoding='utf-8').readlines()[0].split(";")
     with open(name, 'r', encoding='utf-8') as exerciseFile:
-     #   lines = exerciseFile.readlines()
         exerciseHeader = exerciseFile.readline()
         exerciseLines = exerciseFile.readlines()
+    print(exerciseHeader)
+    exercise = exerciseLines[0]
+    exerciseQuestion = exercise.split(";")[0].strip()
+    exerciseAnswerOne = exercise.split(";")[1].strip()
+    exerciseAnswerTwo = exercise.split(";")[2].strip()
+    rightAnswer = exercise.split(";")[3].strip()
+
+
     app.setLabel("header", exerciseHeader)
+    app.setLabel("main_phrase", exerciseQuestion)
+
+    app.setButton("first_answer_button", exerciseAnswerOne)
+    app.setButton("second_answer_button", exerciseAnswerTwo)
+
+
 def exercise(button):
     global lineNumber
     global rightAnswer
-    global wrongAnswer
-    global exerciseHeader
     global exerciseLines
     global rightAnswerCount
     global wrongAnswerCount
-    
-    if button == "Выбрать":
-        print(open(name, 'r', encoding='utf-8').readlines()[0])
-        app.setLabel("main_phrase", exerciseHeader)
-        app.setButton("ы", exerciseAnswerOne[1]) 	
-        app.setButton("и", exerciseAnswerTwo[2])
-    elif button == "ы" or button == "и":
-       # print(exerciseLines)
-        exercise = exerciseLines[lineNumber]
-        exerciseQuestion = exercise.split(";")[0]
-        exerciseAnswerOne = exercise.split(";")[1]
-        exerciseAnswerTwo = exercise.split(";")[2]
-        exerciseRightAnswer = exercise.split(";")[3]
-        app.setLabel("main_phrase", exerciseQuestion)
-        app.setButton("ы", exerciseAnswerOne)
-        app.setButton("и", exerciseAnswerTwo)
-        lineNumber = lineNumber + 1
-        print(lineNumber, len(exerciseLines))
 
-        if answerButton1 == exerciseRightAnswer:
-            rightAnswerCount = rightAnswerCount + 1
-        else:
-            wrongAnswerCount = wrongAnswerCount + 1
+    button_clicked = app.getButtonWidget(button)['text']
 
-        if lineNumber == len(exerciseLines):
-            print("Test")
-            app.infoBox("Результаты", "Правильных: {} \n Неправильных: {}".format(rightAnswerCount, wrongAnswerCount))
+    print("Right answer: "+rightAnswer, "Your answer: "+button_clicked)
+    if button_clicked == rightAnswer:
+        print("Right!")
+        rightAnswerCount = rightAnswerCount + 1
     else:
-        pass
+        print("Wrong(")
+        wrongAnswerCount = wrongAnswerCount + 1
+
+    questionShuffle = [1, 2]
+    exercise = exerciseLines[lineNumber]
+    exerciseQuestion = exercise.split(";")[0].strip()
+    x = random.choice(questionShuffle)
+    exerciseAnswerOne = exercise.split(";")[x].strip()
+    questionShuffle.remove(x)
+    exerciseAnswerTwo = exercise.split(";")[random.choice(questionShuffle)].strip()
+    rightAnswer = exercise.split(";")[3].strip()
+    app.setLabel("main_phrase", exerciseQuestion)
+    app.setButton("first_answer_button", exerciseAnswerOne)
+    app.setButton("second_answer_button", exerciseAnswerTwo)
+
+    lineNumber = lineNumber + 1
+    print(lineNumber, len(exerciseLines))
+
+    if lineNumber == len(exerciseLines):
+        print("Test")
+        app.infoBox("Результаты", "Правильных: {} \n Неправильных: {}".format(rightAnswerCount, wrongAnswerCount))
+
+
 def press(button):
     app.stop()
  
@@ -96,10 +114,9 @@ app.getLabelWidget("main_phrase").config(font="Times 30", width=30)
 # variants buttons
 app.startPanedFrame("p1")
 
-answerButton1 = exerciseAnswerOne
-answerButton2 = exerciseAnswerTwo
-
-app.addButtons([answerButton1, answerButton2], exercise)
+# app.addButtons(["1", "2"], exercise)
+app.addNamedButton("", "first_answer_button", exercise, row=3, column=4)
+app.addNamedButton("", "second_answer_button", exercise, row=3, column=5)
 app.stopPanedFrame()
  
  
